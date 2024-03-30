bba4486b4def0c884f2fd27b29ec69d64bde58e7
MsVoxxie <odysseylove222@gmail.com>
2024-03-30 19:54:46 -0400

    Kitbashing shit together

---
+++ commands/utility/askKat.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { default: OpenAI } = require('openai');
+
+const OpenAIConfig = new OpenAI({ apiKey: process.env.OPENAI_KEY });
+const AI = OpenAIConfig;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('askkat')
+		.setDescription("Ask Me a question, I'll answer the best I can!")
+		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
+		.addStringOption((str) => str.setName('question').setDescription('What to ask Kat?').setRequired(true))
+		.addBooleanOption((bool) => bool.setName('ephemeral').setDescription('Should this message be hidden? (Default True!)')),
+	options: {
+		devOnly: false,
+		disabled: false,
+	},
+	async execute(client, interaction) {
+		// Ephem Check
+		const ephemeral = interaction.options.getBoolean('ephemeral') || true;
+
+		// Defer, Things take time.
+		await interaction.deferReply({ ephemeral: true });
+
+		// Grab question and setup conversation starter
+		const userQuestion = interaction.options.getString('question');
+		const conversationStarter = [
+			{ role: 'system', content: process.env.PERSONALITY },
+			{ role: 'user', content: userQuestion },
+		];
+
+		// Generate response
+		const aiResponse = await AI.chat.completions.create({
+			model: 'gpt-4',
+			messages: conversationStarter,
+			max_tokens: 1000,
+			temperature: 0.7,
+			frequency_penalty: 0.3,
+			presence_penalty: 0.9,
+			n: 1,
+		});
+
+		let aiReply = aiResponse.choices[0].message?.content;
+
+		await interaction.followUp(aiReply);
+	},
+};
--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -2,13 +2,14 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!').setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+	integration_types: [0, 1, 2],
 	options: {
 		devOnly: false,
 		disabled: false,
 	},
-	async execute(client, interaction, settings) {
+	async execute(client, interaction) {
 		// Defer, Things take time.
-		await interaction.deferReply();
+		await interaction.deferReply({ ephemeral: true });
 
 		await interaction.followUp(`Pong!\n🏓Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 	},

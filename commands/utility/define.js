2997920076e49deacb923e18474384765c657288
MsVoxxie <odysseylove222@gmail.com>
2024-04-23 07:45:26 -0400

    to be without gorm

---
+++ commands/utility/define.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+var wd = require('word-definition');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('define')
+		.setDescription('Search the Dictonary for a definition!')
+		.addStringOption((option) => option.setName('query').setDescription('What would you like to search for?').setRequired(true))
+		.setDMPermission(false)
+		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+	options: {
+		devOnly: false,
+		disabled: false,
+	},
+	async execute(client, interaction, settings) {
+		// Defer, Things take time.
+		await interaction.deferReply();
+
+		// Get options
+		const searchQuery = interaction.options.getString('query').replace(/\s/g, '');
+
+		wd.getDef(searchQuery, 'en', null, function (definition) {
+			if (!definition.definition) return interaction.reply(`No Definition found for \`${searchQuery}\``);
+
+			// Build Embeds
+			const embed = new EmbedBuilder()
+				.setTitle('**Dictionary Search**')
+				.setDescription(`**Search Query»** ${definition.word}`)
+				.addFields({ name: '**Category»**', value: definition.category }, { name: '**Definition»**', value: definition.definition })
+				.setColor(client.color);
+
+			interaction.followUp({ embeds: [embed] });
+		});
+	},
+};
